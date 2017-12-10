dcde04b15c62e49ae96d0fdf66f63c231ead4721
Darkjo <darwinjo98@gmail.com>
2017-12-10 00:56:07 -0600

    Reloj FINAL EL APOCALIPSIS :V

--- ES_Nomina/Form1.Designer.vb
+++ ES_Nomina/Form1.Designer.vb
@@ -26,6 +26,7 @@ Partial Class Form1
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Timer1
@@ -36,7 +37,7 @@ Partial Class Form1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Bernard MT Condensed", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(12, 120)
+        Me.Label1.Location = New System.Drawing.Point(12, 115)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(237, 95)
         Me.Label1.TabIndex = 1
@@ -46,11 +47,22 @@ Partial Class Form1
         '
         Me.Timer2.Enabled = True
         '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Bernard MT Condensed", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(246, 95)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Label2"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(522, 380)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -61,4 +73,5 @@ Partial Class Form1
     Friend WithEvents Timer1 As Timer
     Friend WithEvents Label1 As Label
     Friend WithEvents Timer2 As Timer
+    Friend WithEvents Label2 As Label
 End Class
