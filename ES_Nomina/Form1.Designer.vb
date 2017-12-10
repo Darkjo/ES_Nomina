a36a05e47ed26ccccbe67441b1bf675513706338
Darkjo <darwinjo98@gmail.com>
2017-12-10 00:23:16 -0600

    RELOJ EN FORM1

--- ES_Nomina/Form1.Designer.vb
+++ ES_Nomina/Form1.Designer.vb
@@ -22,7 +22,10 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'CheckBox1
@@ -35,11 +38,26 @@ Partial Class Form1
         Me.CheckBox1.Text = "tuGfa"
         Me.CheckBox1.UseVisualStyleBackColor = True
         '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Bernard MT Condensed", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(29, 142)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(237, 95)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(540, 415)
+        Me.ClientSize = New System.Drawing.Size(522, 380)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.CheckBox1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -49,4 +67,6 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label1 As Label
 End Class
--- ES_Nomina/Form1.vb
+++ ES_Nomina/Form1.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Form1
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Label1.Text = Date.Now.ToLongTimeString
+    End Sub
 
 End Class
