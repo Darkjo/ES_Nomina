ca805434dbd74361fc8650724ace9eae09280e80
Darkjo <darwinjo98@gmail.com>
2017-12-10 00:35:19 -0600

    Dia y mes

--- ES_Nomina/Form1.vb
+++ ES_Nomina/Form1.vb
@@ -3,4 +3,7 @@
         Label1.Text = Date.Now.ToLongTimeString
     End Sub
 
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Label2.Text = DateTime.Now.ToString("dd") + " de " + Format(Now, "MMMM")
+    End Sub
 End Class
