b84673e721ff304f4900ac5c0858c314a76dbaf9
Roger2513 <33108069+Roger2513@users.noreply.github.com>
2017-12-10 22:12:40 -0600

    Poco

--- ES_Nomina/My Project/Resources.Designer.vb
+++ ES_Nomina/My Project/Resources.Designer.vb
@@ -59,15 +59,5 @@ Namespace My.Resources
                 resourceCulture = value
             End Set
         End Property
-        
-        '''<summary>
-        '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
-        '''</summary>
-        Friend ReadOnly Property glass_time_watch_business() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("glass-time-watch-business", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
     End Module
 End Namespace
